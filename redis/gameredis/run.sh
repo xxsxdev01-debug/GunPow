28ca0f5b31f6beb23afebfcd6bcbc9c827a23a1c
Debug <xx.sxdev01@gmail.com>
2026-01-12 00:10:24 +0700

    Update run.sh

--- redis/gameredis/run.sh
+++ redis/gameredis/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./redis-server /ddd/redis/gameredis/conf/redis-8091.conf > /ddd/redis/gameredis/logs/redis-8091.log 2>&1 &
-./redis-server /ddd/redis/gameredis/conf/redis-8092.conf > /ddd/redis/gameredis/logs/redis-8092.log 2>&1 &
-./redis-server /ddd/redis/gameredis/conf/redis-8093.conf > /ddd/redis/gameredis/logs/redis-8093.log 2>&1 &
-./redis-server /ddd/redis/gameredis/conf/redis-8094.conf > /ddd/redis/gameredis/logs/redis-8094.log 2>&1 &
-./redis-server /ddd/redis/gameredis/conf/redis-9091.conf > /ddd/redis/gameredis/logs/redis-9091.log 2>&1 &
-./redis-server /ddd/redis/gameredis/conf/redis-9092.conf > /ddd/redis/gameredis/logs/redis-9092.log 2>&1 &
-./redis-server /ddd/redis/gameredis/conf/redis-9093.conf > /ddd/redis/gameredis/logs/redis-9093.log 2>&1 &
-./redis-server /ddd/redis/gameredis/conf/redis-9094.conf > /ddd/redis/gameredis/logs/redis-9094.log 2>&1 &
+./redis-server /redis/gameredis/conf/redis-8091.conf > /redis/gameredis/logs/redis-8091.log 2>&1 &
+./redis-server /redis/gameredis/conf/redis-8092.conf > /redis/gameredis/logs/redis-8092.log 2>&1 &
+./redis-server /redis/gameredis/conf/redis-8093.conf > /redis/gameredis/logs/redis-8093.log 2>&1 &
+./redis-server /redis/gameredis/conf/redis-8094.conf > /redis/gameredis/logs/redis-8094.log 2>&1 &
+./redis-server /redis/gameredis/conf/redis-9091.conf > /redis/gameredis/logs/redis-9091.log 2>&1 &
+./redis-server /redis/gameredis/conf/redis-9092.conf > /redis/gameredis/logs/redis-9092.log 2>&1 &
+./redis-server /redis/gameredis/conf/redis-9093.conf > /redis/gameredis/logs/redis-9093.log 2>&1 &
+./redis-server /redis/gameredis/conf/redis-9094.conf > /redis/gameredis/logs/redis-9094.log 2>&1 &
