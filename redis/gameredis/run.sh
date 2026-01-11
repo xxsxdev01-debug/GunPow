5bc2a0732ac4e487c3648ffdcd7c5d64cbecf66a
Debug <xx.sxdev01@gmail.com>
2026-01-12 00:46:25 +0700

    Update run.sh

--- redis/gameredis/run.sh
+++ redis/gameredis/run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Tạo thư mục log nếu chưa có để tránh lỗi
-mkdir -p GunPow/redis/gameredis/logs
+# Tạo thư mục log ngay tại chỗ (vì đã cd vào gameredis)
+mkdir -p logs
 
-# Tạo liên kết để lệnh ./ có hiệu lực trên Termux
+# Tạo liên kết lệnh để dùng được ./
 ln -sf $(which redis-server) ./redis-server
 
-# Khởi động các cổng Redis
-./redis-server GunPow/redis/gameredis/conf/redis-8091.conf > GunPow/redis/gameredis/logs/redis-8091.log 2>&1 &
-./redis-server GunPow/redis/gameredis/conf/redis-8092.conf > GunPow/redis/gameredis/logs/redis-8092.log 2>&1 &
-./redis-server GunPow/redis/gameredis/conf/redis-8093.conf > GunPow/redis/gameredis/logs/redis-8093.log 2>&1 &
-./redis-server GunPow/redis/gameredis/conf/redis-8094.conf > GunPow/redis/gameredis/logs/redis-8094.log 2>&1 &
-./redis-server GunPow/redis/gameredis/conf/redis-9091.conf > GunPow/redis/gameredis/logs/redis-9091.log 2>&1 &
-./redis-server GunPow/redis/gameredis/conf/redis-9092.conf > GunPow/redis/gameredis/logs/redis-9092.log 2>&1 &
-./redis-server GunPow/redis/gameredis/conf/redis-9093.conf > GunPow/redis/gameredis/logs/redis-9093.log 2>&1 &
-./redis-server GunPow/redis/gameredis/conf/redis-9094.conf > GunPow/redis/gameredis/logs/redis-9094.log 2>&1 &
+# Khởi động các cổng Redis bằng đường dẫn tương đối
+./redis-server conf/redis-8091.conf > logs/redis-8091.log 2>&1 &
+./redis-server conf/redis-8092.conf > logs/redis-8092.log 2>&1 &
+./redis-server conf/redis-8093.conf > logs/redis-8093.log 2>&1 &
+./redis-server conf/redis-8094.conf > logs/redis-8094.log 2>&1 &
+./redis-server conf/redis-9091.conf > logs/redis-9091.log 2>&1 &
+./redis-server conf/redis-9092.conf > logs/redis-9092.log 2>&1 &
+./redis-server conf/redis-9093.conf > logs/redis-9093.log 2>&1 &
+./redis-server conf/redis-9094.conf > logs/redis-9094.log 2>&1 &
 
-echo "--- Đã gửi lệnh khởi động 8 cổng Redis ---"
+echo "--- Đã gửi lệnh khởi động 8 cổng GameRedis ---"
