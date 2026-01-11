7b7af40de3964b31acb66dab3cd737ee33f89e8f
Debug <xx.sxdev01@gmail.com>
2026-01-12 00:30:04 +0700

    Update run.sh

--- redis/ipdredis/run.sh
+++ redis/ipdredis/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-./redis-server /redis/ipdredis/conf/redis-8071.conf > /redis/ipdredis/logs/redis-8071.log 2>&1 &
-./redis-server /redis/ipdredis/conf/redis-8072.conf > /redis/ipdredis/logs/redis-8072.log 2>&1 &
-./redis-server /redis/ipdredis/conf/redis-8073.conf > /redis/ipdredis/logs/redis-8073.log 2>&1 &
-./redis-server /redis/ipdredis/conf/redis-8074.conf > /redis/ipdredis/logs/redis-8074.log 2>&1 &
-./redis-server /redis/ipdredis/conf/redis-9071.conf > /redis/ipdredis/logs/redis-9071.log 2>&1 &
-./redis-server /redis/ipdredis/conf/redis-9072.conf > /redis/ipdredis/logs/redis-9072.log 2>&1 &
-./redis-server /redis/ipdredis/conf/redis-9073.conf > /redis/ipdredis/logs/redis-9073.log 2>&1 &
-./redis-server /redis/ipdredis/conf/redis-9074.conf > /redis/ipdredis/logs/redis-9074.log 2>&1 &
+# Tạo liên kết để lệnh ./ có hiệu lực trên Termux
+ln -sf $(which redis-server) ./redis-server
+
+./redis-server GunPow/redis/ipdredis/conf/redis-8071.conf > GunPow/redis/ipdredis/logs/redis-8071.log 2>&1 &
+./redis-server GunPow/redis/ipdredis/conf/redis-8072.conf > GunPow/redis/ipdredis/logs/redis-8072.log 2>&1 &
+./redis-server GunPow/redis/ipdredis/conf/redis-8073.conf > GunPow/redis/ipdredis/logs/redis-8073.log 2>&1 &
+./redis-server GunPow/redis/ipdredis/conf/redis-8074.conf > GunPow/redis/ipdredis/logs/redis-8074.log 2>&1 &
+./redis-server GunPow/redis/ipdredis/conf/redis-9071.conf > GunPow/redis/ipdredis/logs/redis-9071.log 2>&1 &
+./redis-server GunPow/redis/ipdredis/conf/redis-9072.conf > GunPow/redis/ipdredis/logs/redis-9072.log 2>&1 &
+./redis-server GunPow/redis/ipdredis/conf/redis-9073.conf > GunPow/redis/ipdredis/logs/redis-9073.log 2>&1 &
+./redis-server GunPow/redis/ipdredis/conf/redis-9074.conf > GunPow/redis/ipdredis/logs/redis-9074.log 2>&1 &
